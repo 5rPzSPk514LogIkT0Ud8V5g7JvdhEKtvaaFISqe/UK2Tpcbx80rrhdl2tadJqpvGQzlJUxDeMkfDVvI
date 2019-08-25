b5a41fac6b9b999919f7e0e69fe8677ec5eb1b57
5rPzSPk514LogIkT0Ud8V5g7JvdhEKtvaaFISqe <53229917+5rPzSPk514LogIkT0Ud8V5g7JvdhEKtvaaFISqe@users.noreply.github.com>
2019-08-25 18:12:56 +0300

    a

---
+++ nEJzAopuBZGFg3WP0vm3LrCbjap3dqm86GHrpnm/wNvOrmihYrlAas151Ka9HXAUDYBCsnSJATV6VvO/Qfqen744nmIl0WhSXzTI1NSKRiLfaaFVNPRBOI4/Aq0rxfD1ejuH76kZ3ju6S9WThDIv02zyXNoE3GQ/JGqQi4nXjkMc7Xk43wfqcwCi.lua
@@ -0,0 +1,14 @@
+print('lmao')
+lI1lIIlII1 = "."
+lllllIllII = "Unknown"
+II1ll1lII1 = " "
+lI11lI1ll1 = "[SYSTEM]"
+I1II1IIIII = "is"
+IlII11Il1I = "StarterGui"
+lllllIlllI = "by "
+IllllII1ll = "ChatMakeSystemMessage"
+function llIllI1III(Text, Color)
+	game:GetService(IlII11Il1I):SetCore(IllllII1ll, {Text = Text, Color = Color})
+	print(Text)
+end
+llIllI1III(lI11lI1ll1..II1ll1lII1.."This"..II1ll1lII1.."game"..II1ll1lII1..I1II1IIIII..II1ll1lII1.."protected"..II1ll1lII1..lllllIlllI..lllllIllII..lI1lIIlII1, Color3.new(0, 236, 0))
